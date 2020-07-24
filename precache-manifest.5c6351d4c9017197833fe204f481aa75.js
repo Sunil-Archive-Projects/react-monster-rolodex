22d19ec13dfd13f40bbe47e8452ffe54dff79fc1
Sunil L <sunilandroidnayak@gmail.com>
2020-07-25 01:14:47 +0530

    Updates

---
+++ precache-manifest.5c6351d4c9017197833fe204f481aa75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957941ee5b840ded3ec3dcb84f3dc859",
+    "url": "/react-monster-rolodex/index.html"
+  },
+  {
+    "revision": "aad3ba04efabdabcc471",
+    "url": "/react-monster-rolodex/static/css/main.b89b6bc0.chunk.css"
+  },
+  {
+    "revision": "9c0a05a0f874d8d151ae",
+    "url": "/react-monster-rolodex/static/js/2.32c3ed0e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monster-rolodex/static/js/2.32c3ed0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad3ba04efabdabcc471",
+    "url": "/react-monster-rolodex/static/js/main.8cbc18cd.chunk.js"
+  },
+  {
+    "revision": "ee29001f23a3f777f392",
+    "url": "/react-monster-rolodex/static/js/runtime-main.a2ea08f6.js"
+  }
+]);
