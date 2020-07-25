b866a609f5bc39bfe8020eb58a6e7f9c1439e217
Sunil L <sunilandroidnayak@gmail.com>
2020-07-25 21:36:44 +0530

    Added the Card Component

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -1,1 +1,11 @@
 import React from 'react';
+import './card.styles.css';
+
+export const Card = (props) => (
+    <div className="card-container">
+        { console.log(props) }
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set1&size=180x200`}></img>
+        <h1>{props.monster.name}</h1>
+        <h3>{props.monster.email}</h3>
+    </div>
+);
