e1da7d24f742fcdeaf6e34a6e25d8b9638b3afcc
Sunil L <sunilandroidnayak@gmail.com>
2020-07-27 18:40:41 +0530

    Final Commit for the Bender Rolodex

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,7 @@ import './card.styles.css';
 export const Card = (props) => (
     <div className="card-container">
         <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set3&size=180x200`}></img>
-        <h1>{props.monster.name}</h1>
+        <h2>{props.monster.name}</h2>
         <h5>{props.monster.email}</h5>
     </div>
 );
